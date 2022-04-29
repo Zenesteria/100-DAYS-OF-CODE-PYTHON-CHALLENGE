e07c8fcd6cfab905a0822046c8fac4c6bf5c96e5
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-04-29 15:09:44 +0100

    Day - 1 Variables

--- Coding Bootcamp/Day-1.py
+++ Coding Bootcamp/Day-1.py
@@ -8,3 +8,10 @@ print('Hello my name is abdurrahman\nI\'m Nigerian\nand I\'m learning python')
 print('Hello ' + 'Abdurrahman')
 
 # Input Function
+print('Hello there\n' + input('What is your name? ') + '!')
+
+# Variables
+name = input('What is your name? ')
+length = len(name)
+
+print('Hello! ' + name +  ' [%s]'%length)
