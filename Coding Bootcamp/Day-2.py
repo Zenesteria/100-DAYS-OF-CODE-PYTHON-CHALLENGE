4762752c3c61164b3d478af208a71cbcc63d9b0e
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-04-30 07:03:03 +0100

    Day - 2 Integers

---
+++ Coding Bootcamp/Day-2.py
@@ -0,0 +1,9 @@
+#Data Types
+
+# Strings
+print('Hello'[4])
+
+# Integers - Whole numbers either positive or negative are called integers
+print(123456 + 345678)
+# The integers can be seperated with an _ to represent a , in the human notation of a number
+print(123_456 + 345_678)
