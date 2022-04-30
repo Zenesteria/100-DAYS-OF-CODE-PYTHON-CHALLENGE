009299a2396e357d0e21ba4b03d4a41e6fedb0b3
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-04-30 07:32:24 +0100

    Day - 2 Data Casting/Conversion Floats & Booleans

--- Coding Bootcamp/Day-2.py
+++ Coding Bootcamp/Day-2.py
@@ -7,3 +7,22 @@ print('Hello'[4])
 print(123456 + 345678)
 # The integers can be seperated with an _ to represent a , in the human notation of a number
 print(123_456 + 345_678)
+
+# Float - A floating point number is a number with a floating point (.) within it, i.e a decimal number
+floatNumber = 12.345
+
+# Boolean
+condition1 = True
+condition2 = False
+
+# Data casting/conversion
+# types
+a = '123'
+type(a) #str
+
+b = 123
+type(b) #int
+
+no_of_char = str(len(input('What is your name?\n')))
+print('Your name has ' + no_of_char + ' characters.')
+
