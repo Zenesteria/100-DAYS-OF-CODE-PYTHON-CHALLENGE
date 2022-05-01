f0031c4c010708d3410f34f3b878fe4456601a2d
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-05-01 16:29:04 +0100

    Day-3 Control Flow with If else statements

---
+++ Coding Bootcamp/Day-3.py
@@ -0,0 +1,6 @@
+# Control flow with If/else statements
+height = int(input('What is your height in cm? '))
+if height > 130:
+    print('you can go on the ride, here\'s a ticket!')
+else:
+    print('Sorry kid, too short')
