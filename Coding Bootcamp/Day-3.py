eb8f4066f489e1ef48ace58a1f7bd9e816e8d96d
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-05-01 16:50:23 +0100

    Nested if statements and elif statement

--- Coding Bootcamp/Day-3.py
+++ Coding Bootcamp/Day-3.py
@@ -1,6 +1,12 @@
 # Control flow with If/else statements
 height = int(input('What is your height in cm? '))
-if height > 130:
-    print('you can go on the ride, here\'s a ticket!')
+if height >= 120:
+    age = int(input('How old are you? '))
+    if age < 12:
+        print('you have to pay $5')
+    elif age <= 18:
+        print('you have to pay $7')
+    else:
+        print('you have to pay $12')
 else:
     print('Sorry kid, too short')
