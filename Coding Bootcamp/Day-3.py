288e805fea764f0b10cbd9aa3c0c1903ab7affd9
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-05-01 17:30:03 +0100

    Multiple If statements

--- Coding Bootcamp/Day-3.py
+++ Coding Bootcamp/Day-3.py
@@ -1,12 +1,22 @@
 # Control flow with If/else statements
 height = int(input('What is your height in cm? '))
 if height >= 120:
+    Bill = 0
     age = int(input('How old are you? '))
     if age < 12:
+        Bill = 5
         print('you have to pay $5')
     elif age <= 18:
+        Bill = 7
         print('you have to pay $7')
     else:
+        Bill = 12
         print('you have to pay $12')
+    
+    # Check if they need a photo taken
+    wants_photo_taken = input('Do you want a photo taken? Y or N ')
+    if wants_photo_taken == 'Y':
+        Bill += 3
+    print(f'Your final bill is ${Bill}')
 else:
     print('Sorry kid, too short')
